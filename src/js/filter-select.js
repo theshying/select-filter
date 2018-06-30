682891048640557fdd8a8b7bf5b2c0c8a2985eb8
theShy <969153746@qq.com>
2018-06-30 23:37:53 +0800

    :racehorse:  改进代码结构

--- src/js/filter-select.js
+++ src/js/filter-select.js
@@ -2,44 +2,84 @@
     var filterSelect = function (ele, opt) {
         this.$el = ele;
         this.default = {
+            //下拉数据
             data: [],
-            value: this.$el.val()
+            //默认选中值
+            value: this.$el.val(),
+            //单选或复选（'radio' || 'checkbox'）
+            type: 'radio',
+            placeholder: '请选择',
+            callback: {
+                selected: function(){},
+            },
+
         };
         this.option = $.extend({}, this.default, opt);
     };
     filterSelect.prototype = {
+        //初始化
         init() {
+            this.$el.text(this.option.placeholder);
             var list = this.option.data;
-            var html = '<span class="select"></span><div class="container"><input class="select-input" type="text" class="block"> <ul class="container-list">';
+            var html = '<div class="drop-down__container"><input class="container-search__input" type="text" class="block"> <ul class="container-list__all">';
             list.forEach(function (item) {
                 if (item.value == this.option.value) {
-                    this.setSelect(item.value, item.label)
-                    html += '<li class="container-item current-select" value="' + item.value + '">' + item.label + '</li>'
+                    this.setSelect(item.label)
+                    html += '<li class=container-list__item current-select" value="' + item.value + '">' + item.label + '</li>'
                 }
-                html += '<li class="container-item" value="' + item.value + '">' + item.label + '</li>'
+                html += '<li class="container-list__item" value="' + item.value + '">' + item.label + '</li>'
             }.bind(this))
             html += '</ul></div>'
             this.$el.after(html);
-            this.$el.parent().find('.select').on('click', function () {
-                this.trigger()
-            }.bind(this))
-            this.$el.parent().find('.select-input').on('input', function (e) {
-                this.filter(e.target.value)
-            }.bind(this))
-            $('.container-item').on('click', function (e) {
-                this.setSelect(e.target.value, e.target.innerText)
-            }.bind(this))
-
+            this.getElement();
+            this.eventRegister();
         },
-        trigger() {
-            var container = this.$el.parent().find('.container');
-            container.css('display', container.css('display') === 'block' ? 'none' : 'block')
+        //获取dom元素
+        getElement() {
+            var el = this.$el.parent().children();
+            this.$els = {
+                //选中元素显示区域
+                content_el:$(el[0]),
+                //下拉列表容器
+                container_el:$(el[1]),
+                // 下拉列表元素
+                list_els:$(el[1]).children().children(),
+                // 检索框
+                search_el: $(el[1]).children('.container-search__input')
+            }
+        },
+        //事件绑定
+        eventRegister() {
+            this.$els.content_el.on('click', function () {
+                this.trigger();
+                this.$els.search_el.trigger('focus')
+            }.bind(this))
+             this.$els.search_el.on('input', function (e) {
+                 this.filter(e.target.value)
+             }.bind(this))
+            this.$els.container_el.on('click', function (e) {
+                 this.option.callback.selected({
+                     value: e.target.value,
+                     name: e.target.innerText,
+                 })
+                 this.setSelect(e.target.innerText)
+                 this.trigger(false)
+             }.bind(this))
+        },
+        trigger(show) {
+            var display = null
+            if (show === undefined) {
+                display = this.$els.container_el.css('display') === 'block' ?  'none' : 'block';
+            } else {
+                display = show === true ? 'block' : 'none'
+            }
+            this.$els.container_el.css('display', display)
         },
         filter(string) {
-            var list = $('.container-item');
-            $.makeArray(list).forEach(function (item) {
+          var el =this.$els.list_els
+            $.makeArray(el).forEach(function (item) {
                 if (string === '') {
-                    list.css('display', 'block')
+                    el.css('display', 'block')
                 } else {
                     if (item.innerText.indexOf(string) !== -1) {
                         $(item).css('display', 'block')
@@ -49,10 +89,8 @@
                 }
             })
         },
-        setSelect(val, text) {
-            $('.select').text(text);
-            this.$el.val(val);
-            // this.trigger()
+        setSelect(name) {
+            this.$el.text(name);
         }
     };
 
